7ea03936ec5199a4dae5497020525b558f5fd01d
Noah <noah@magnuss-mbp.myfiosgateway.com>
2020-12-15 11:53:28 -0500

    updated text

--- src/stories/0-introduction.stories.js
+++ src/stories/0-introduction.stories.js
@@ -1,3 +1,4 @@
+import "@tailwindcss/postcss7-compat/base.css"
 import "tailwindcss/tailwind.css"
 
 export default {
@@ -5,8 +6,9 @@ export default {
 }
 
 export const purpose = () => ({
-  template: `<div class="container mx-auto p-6">
-    <h1 class="font-bold mb-6 text-3xl">Introduction</h1>
-    <p>The purpose of this document is to provide documentation with examples for maintaining compliance with the WCAG 2.1</p>
+  template: `<div class="container mx-auto p-9">
+    <h1 class="font-bold mb-6 text-5xl">Introduction</h1>
+    <p>The purpose of this document is to provide designs and developers with a deeper understanding of how to write accessible code. This site is completely extendable is inteded to be a living document.</p>
+    <p>Where ever possible I will try to provide the specific WCAG 2.1 success criteria and rating level as it relates to each element.</p>
   </div>`
 })
